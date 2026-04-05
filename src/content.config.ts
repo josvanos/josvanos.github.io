aef85d409eaad30a8b94afc47d175d0e0dcb0fed
Noah <m.n.vanos@students.uu.nl>
2026-04-05 15:50:56 +0200

    cleanup of template

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,14 @@
-import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
+import { defineCollection } from 'astro:content';
+
+const referenceSchema = z.object({
+	title: z.string().optional(),
+	url: z.string().optional(),
+	author: z.string().optional(),
+	number: z.number().optional(),
+});
+
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -11,9 +19,10 @@ const blog = defineCollection({
 			title: z.string(),
 			description: z.string(),
 			// Transform string to Date object
-			pubDate: z.coerce.date(),
+			pubDate: z.coerce.date().optional(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			references: z.array(referenceSchema).optional(),
 		}),
 });
 
